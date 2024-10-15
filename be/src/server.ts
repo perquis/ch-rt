d05485bf8b4ffca73a5c9d4941e45f4f5b29166b
perquis <thetahirpl@gmail.com>
2024-10-15 23:50:19 +0200

    docker(docker-compose.yml): Added docker files to run backend app and mongodb.

--- be/src/server.ts
+++ be/src/server.ts
@@ -1,10 +1,12 @@
 import { v1 } from '@/router/v1';
+import { connectToMongoDB } from '@/services/mongodb';
 import dotenv from 'dotenv';
 import express from 'express';
 import morgan from 'morgan';
 
 const app = express();
 dotenv.config();
+connectToMongoDB();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
@@ -12,6 +14,10 @@ app.use(express.urlencoded({ extended: true }));
 app.use(morgan('dev'));
 
 app.use('/api/v1', v1);
+
+app.get('/healthcheck', (_req, res) => {
+  res.send({ message: 'Ok' });
+});
 
 if (process.env.NODE_ENV !== 'production' && process.env.NODE_ENV !== 'development') {
   console.error('The NODE_ENV must be either production or development.');
