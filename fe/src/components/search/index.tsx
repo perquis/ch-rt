015fe687d9d67f3976095e531eaabe50f3476c3f
perquis <thetahirpl@gmail.com>
2024-10-16 23:55:53 +0200

    a11y(searchbar): Added delaying when the user types keywords to this component.

--- fe/src/components/search/index.tsx
+++ fe/src/components/search/index.tsx
@@ -1,19 +1,26 @@
-import { ChangeEvent } from 'react';
+import { ChangeEvent, useEffect, useState } from 'react';
 import { useFilterContext } from '../../contexts/filters';
 
 export const Search = () => {
-  const { query, setQuery } = useFilterContext();
+  const { setQuery } = useFilterContext();
+  const [inputValue, setInputValue] = useState('');
 
   const handleChange = (event: ChangeEvent<HTMLInputElement>) => {
-    const { value } = event.target;
-    setQuery(value);
+    setInputValue(event.target.value);
   };
+
+  useEffect(() => {
+    const timeoutId = setTimeout(() => {
+      setQuery(inputValue);
+    }, 500);
+    return () => clearTimeout(timeoutId);
+  }, [inputValue, 500]);
 
   return (
     <input
       type="search"
       placeholder={'Search'}
-      value={query}
+      value={inputValue}
       onChange={handleChange}
       className={'text-sm font-normal px-3 py-2 bg-white w-full'}
     />
